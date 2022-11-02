8875c723732cc55afb6f2d4ca99a266a2131492d
fr4nkln11 <ikehfranklind3c0d3r@gmail.com>
2022-11-02 02:44:57 +0100

    fixed input glow

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('.btn-close').forEach(button => {
         button.onclick = () => {
             const request = new XMLHttpRequest();
@@ -10,5 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
             request.send();
         };
     });
+});
+
+document.querySelector('#add_task').addEventListener('click', () => {
+    const new_entry = document.querySelector('#new_entry').value;
     
+    const request = new XMLHttpRequest();
+    request.open('POST', `/create/${new_entry}`);
+    request.onload = () => {
+        const response = request.responseText;
+        alert(response)
+    }
+    request.send();
 });
