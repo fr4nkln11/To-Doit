effbce5072615487a6c58892d68f4c7463e4ef9a
fr4nkln11 <ikehfranklind3c0d3r@gmail.com>
2022-11-15 18:34:49 +0100

    flask debug

--- settings.py
+++ settings.py
@@ -1,9 +1,7 @@
 import os
 
 FLASK_APP = os.environ["FLASK_APP"]
-FLASK_ENV = os.environ["FLASK_ENV"]
+FLASK_DEBUG = os.environ["FLASK_DEBUG"]
 SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URI"]
 SECRET_KEY = os.environ["SECRET_KEY"]
-
-DEBUG = False
 SQLALCHEMY_ECHO = False
